cca5b47f56f48852a5184294c00a254b6c13221e
Melina <mrequena@fue.undef.edu.ar>
2023-03-30 14:57:14 -0300

    Break/Continue practice

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
+#include<stdio.h>
+ int main()
+ {
+    int i, num;
+    do {
+        printf("Ingrese un numero entre 1 y 20, que quiere skippear");
+        scanf("%d", &num);
+    }while(num>20 || num<1);
+    for(i=1; i<=20; i++)
+    {
+        if(i==num)
+        {
+            break;
+        }
+        printf("%d \n", i);
+    }
     return 0;
-}
+ }

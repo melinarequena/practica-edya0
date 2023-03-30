239800c811a62cca5043b42857411638a6533c19
Melina <mrequena@fue.undef.edu.ar>
2023-03-30 15:27:52 -0300

    Valor de la hipotenusa

--- main.c
+++ main.c
@@ -1,18 +1,13 @@
+
 #include<stdio.h>
- int main()
- {
-    int i, num;
-    do {
-        printf("Ingrese un numero entre 1 y 20, que quiere skippear");
-        scanf("%d", &num);
-    }while(num>20 || num<1);
-    for(i=1; i<=20; i++)
-    {
-        if(i==num)
-        {
-            break;
-        }
-        printf("%d \n", i);
-    }
+int main()
+{
+    float a, b, h;
+    printf("Ingrese el valor del cateto a:\n");
+    scanf("%f", &a);
+    printf("Ingrese valor del cateto b:\n");
+    scanf("%f", &b);
+    h=(a*a+b*b);
+    printf("El valor de la hipotenusa es %.2f", h);
     return 0;
- }
+}
